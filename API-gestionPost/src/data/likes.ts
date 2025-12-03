2ff45fadb5abd818b53c847cd57ad2e5134b0ef0
lex <josalexander25@gmail.com>
2025-12-03 17:38:21 -0500

    version 1.7

---
+++ API-gestionPost/src/data/likes.ts
@@ -0,0 +1,14 @@
+import { query } from "../lib/db.ts";
+
+// agregar like y devolveremos el post actualizado con likesCount
+export async function addLike(postId:number, userId:number) {
+    // aqui se guarda la interacion en la tabla likes
+    const sql = 'INSERT INTO likes (postId, userId) VALUES ($1, $2) ON CONFLICT DO NOTHING RETURNING *';
+    await query(sql,[postId, userId]);
+
+    // Devolvemos el post con likesCount actualizado
+    const getSql = 'SELECT id, userId, content, createdat,(SELECT COUNT(*) FROM likes WHERE postId = $1)::INT as "likesCount" FROM posts WHERE id = $1';
+    const result = await query(getSql,[postId]);
+    return result.rows[0]
+}
+
